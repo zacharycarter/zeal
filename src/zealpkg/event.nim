b392bdf59440d273e900f963dd568c4712553317
carterza <carterza@gmail.com>
2019-07-03 07:21:48 +0300

    more event module work

--- src/zealpkg/event.nim
+++ src/zealpkg/event.nim
@@ -2,6 +2,20 @@ import sdl2 as sdl
 
 type
   EventType* {.size: sizeof(uint32).} = enum
+    #
+    # +-----------------+-----------------------------------------------+
+    # | Range           | Use                                           |
+    # +-----------------+-----------------------------------------------+
+    # | 0x0-0xffff      | SDL events                                    |
+    # +-----------------+-----------------------------------------------+
+    # | 0x10000-0x1ffff | Engine-generated events                       |
+    # +-----------------+-----------------------------------------------+
+    # | 0x20000-0x2ffff | Script-generated events                       |
+    # +-----------------+-----------------------------------------------+
+    #
+    # The very first event serviced during a tick is a single EVENT_UPDATE_START one.
+    # The very last event serviced during a tick is a single EVENT_UPDATE_END one. 
+    #
     etUpdateStart = uint32(sdl.LastEvent) + 1,
     etUpdateEnd,
     etUpdateUI,
