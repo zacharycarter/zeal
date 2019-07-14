1e06f8b978729c223bb93385afe91084e0365c16
carterza <carterza@gmail.com>
2019-07-14 15:05:53 +0300

    more work on map rendering

--- src/zealpkg/tile.nim
+++ src/zealpkg/tile.nim
@@ -77,16 +77,172 @@ type
   Face* = object
     nw, ne, se, sw: Vertex
 
+proc tileNWHeight(tile: Tile): int =
+  let topNWRaised = tile.kind == tkRampSN or
+                    tile.kind == tkRampEW or
+                    tile.kind == tkCornerConvexSW or
+                    tile.kind == tkCornerConvexSE or
+                    tile.kind == tkCornerConcaveSE or
+                    tile.kind == tkCornerConvexNE
+  
+  if topNWRaised:
+    result = tile.baseHeight + tile.rampHeight
+  else:
+    result = tile.baseHeight
+
+proc tileNEHeight(tile: Tile): int =
+  let topNERaised = tile.kind == tkRampSN or
+                    tile.kind == tkRampWE or
+                    tile.kind == tkCornerConvexSW or
+                    tile.kind == tkCornerConcaveSW or
+                    tile.kind == tkCornerConvexSE or
+                    tile.kind == tkCornerConvexNW
+  
+  if topNERaised:
+    result = tile.baseHeight + tile.rampHeight
+  else:
+    result = tile.baseHeight
+
+proc tileSWHeight(tile: Tile): int =
+  let topSWRaised = tile.kind == tkRampNS or
+                    tile.kind == tkRampEW or
+                    tile.kind == tkCornerConvexSE or
+                    tile.kind == tkCornerConvexNW or
+                    tile.kind == tkCornerConcaveNE or
+                    tile.kind == tkCornerConvexNE
+  
+  if topSWRaised:
+    result = tile.baseHeight + tile.rampHeight
+  else:
+    result = tile.baseHeight
+
+proc tileSEHeight(tile: Tile): int =
+  let topSERaised = tile.kind == tkRampNS or
+                    tile.kind == tkRampWE or
+                    tile.kind == tkCornerConvexSW or
+                    tile.kind == tkCornerConvexNE or
+                    tile.kind == tkCornerConcaveNW or
+                    tile.kind == tkCornerConvexNW
+  
+  if topSERaised:
+    result = tile.baseHeight + tile.rampHeight
+  else:
+    result = tile.baseHeight
+
 proc getTileVertices*(tile: Tile, vertices: var seq[Vertex], r, c: int) =
   # Bottom face is always the same (just shifted over based on row and column), and the 
   # front, back, left, right faces just connect the top and bottom faces. The only 
   # variations are in the top face, which has some corners raised based on tile type. 
   #
-  let faceBot = Face(
+  let bot = Face(
     nw: Vertex(
       pos: newVec3(0.0 - float32((c+1) * xCoordsPerTile), (-1.0 * yCoordsPerTile), 0.0 + float32(r * zCoordsPerTile)),
       uv: newVec2(0.0, 1.0),
       normal: newVec3(0.0, -1.0, 0.0),
       materialIdx: tile.topMatIdx
+    ),
+    ne: Vertex(
+      pos: newVec3(0.0 - float32(c * xCoordsPerTile), (-1.0 * yCoordsPerTile), 0.0 + float32(r * zCoordsPerTile)),
+      uv: newVec2(1.0, 1.0),
+      normal: newVec3(0.0, -1.0, 0.0),
+      materialIdx: tile.topMatIdx
+    ),
+    se: Vertex(
+      pos: newVec3(0.0 - float32(c * xCoordsPerTile), (-1.0 * yCoordsPerTile), 0.0 + float32((r+1) * zCoordsPerTile)),
+      uv: newVec2(1.0, 0.0),
+      normal: newVec3(0.0, -1.0, 0.0),
+      materialIdx: tile.topMatIdx
+    ),
+    sw: Vertex(
+      pos: newVec3(0.0 - float32((c+1) * xCoordsPerTile), (-1.0 * yCoordsPerTile), 0.0 + float32((r+1) * zCoordsPerTile)),
+      uv: newVec2(0.0, 0.0),
+      normal: newVec3(0.0, -1.0, 0.0),
+      materialIdx: tile.topMatIdx
+    )
+  )
+
+  # Normals for top face get set at the end
+  let top = Face(
+    nw: Vertex(
+      pos: newVec3(0.0 - float32(c * xCoordsPerTile), float32(tileNWHeight(tile) * yCoordsPerTile), 0.0 + float32(r * zCoordsPerTile)),
+      uv: newVec2(0.0, 1.0),
+      materialIdx: tile.topMatIdx
+    ),
+    ne: Vertex(
+      pos: newVec3(0.0 - float32((c+1) * xCoordsPerTile), float32(tileNEHeight(tile) * yCoordsPerTile), 0.0 + float32(r * zCoordsPerTile)),
+      uv: newVec2(1.0, 1.0),
+      materialIdx: tile.topMatIdx
+    ),
+    se: Vertex(
+      pos: newVec3(0.0 - float32((c+1) * xCoordsPerTile), float32(tileSEHeight(tile) * yCoordsPerTile), 0.0 + float32((r+1) * zCoordsPerTile)),
+      uv: newVec2(1.0, 0.0),
+      materialIdx: tile.topMatIdx
+    ),
+    sw: Vertex(
+      pos: newVec3(0.0f - float32(c * xCoordsPerTile), float32(tileSWHeight(tile) * yCoordsPerTile), 0.0f + float32((r+1) * zCoordsPerTile)),
+      uv: newVec2(0.0, 0.0),
+      materialIdx: tile.topMatIdx
+    )
+  )
+
+  template vCoord(width, height: untyped): untyped =
+    ((cast[float32](height)) / width)
+
+  let sideAdjacentIndices = ((tile.sidesMatIdx and 0xf) shl 0) or 
+                            ((tile.sidesMatIdx and 0xf) shl 4) or 
+                            ((tile.sidesMatIdx and 0xf) shl 8) or 
+                            ((tile.sidesMatIdx and 0xf) shl 12)
+
+  let back = Face(
+    nw: Vertex(
+      pos: top.ne.pos,
+      uv: newVec2(0.0, vCoord(xCoordsPerTile, top.ne.pos.y)),
+      normal: newVec3(0.0, 0.0, -1.0),
+      materialIdx: tile.sidesMatIdx
+    ),
+    ne: Vertex(
+      pos: top.nw.pos,
+      uv: newVec2(1.0, vCoord(xCoordsPerTile, top.nw.pos.y)),
+      normal: newVec3(0.0, 0.0, -1.0),
+      materialIdx: tile.sidesMatIdx
+    ),
+    se: Vertex(
+      pos: bot.ne.pos,
+      uv: newVec2(1.0, 0.0),
+      normal: newVec3(0.0, 0.0, -1.0),
+      materialIdx: tile.sidesMatIdx
+    ),
+    sw: Vertex(
+      pos: bot.nw.pos,
+      uv: newVec2(0.0, 0.0),
+      normal: newVec3(0.0, 0.0, -1.0),
+      materialIdx: tile.sidesMatIdx
+    )
+  )
+
+  let front = Face(
+    nw: Vertex(
+      pos: top.sw.pos,
+      uv: newVec2(0.0, vCoord(xCoordsPerTile, top.sw.pos.y)),
+      normal: newVec3(0.0, 0.0, 1.0),
+      materialIdx: tile.sidesMatIdx
+    ),
+    ne: Vertex(
+      pos: top.se.pos,
+      uv: newVec2(1.0, vCoord(xCoordsPerTile, top.se.pos.y)),
+      normal: newVec3(0.0, 0.0, 1.0),
+      materialIdx: tile.sidesMatIdx
+    ),
+    se: Vertex(
+      pos: bot.sw.pos,
+      uv: newVec2(1.0, 0.0),
+      normal: newVec3(0.0, 0.0, 1.0),
+      materialIdx: tile.sidesMatIdx
+    ),
+    sw: Vertex(
+      pos: bot.se.pos,
+      uv: newVec2(0.0, 0.0),
+      normal: newVec3(0.0, 0.0, 1.0),
+      materialIdx: tile.sidesMatIdx
     )
   )
